09a367911031a5abc3d4359f64f5dfa4e2b39f0e
Bhanu Partap <bhanupartap25275@gmail.com>
2025-04-03 02:55:08 +0530

    feat: Enhance create-wrapped script to fetch Custom ERC-20 Token ID and support native tokens   (#11)
    * refactor: update create-wrapped script to use ERC-20 token ID and adjust chain references
    
    * refactor: update ERC-20 token address placeholder and remove debug logs in create-wrapped script
    
    * updated the token address
    
    * feat: update create-wrapped script to include Solana and Sui chains and modify ERC-20 token address placeholder

--- src/scripts/create-wrapped.ts
+++ src/scripts/create-wrapped.ts
@@ -1,4 +1,4 @@
-import { Wormhole, signSendWait, wormhole } from '@wormhole-foundation/sdk';
+import { Wormhole, signSendWait, wormhole, TokenId } from '@wormhole-foundation/sdk';
 import evm from '@wormhole-foundation/sdk/evm';
 import solana from '@wormhole-foundation/sdk/solana';
 import sui from '@wormhole-foundation/sdk/sui';
@@ -7,24 +7,33 @@ import { getSigner } from '../helpers/helpers';
 
 (async function () {
 	const wh = await wormhole('Testnet', [evm, solana, sui]);
+	console.log("🚀 ~ create-wrapped.ts:10 ~ wh:", wh)
 
 	// Define the source and destination chains
-	const origChain = wh.getChain('ArbitrumSepolia');
-	// funds on the destination chain needed!
-	const destChain = wh.getChain('BaseSepolia');
+	const origChain = wh.getChain('Avalanche');
 
-	// Retrieve the token ID from the source chain
-	const token = await origChain.getNativeWrappedTokenId();
+	// funds on the destination chain needed!
+	const destChain = wh.getChain('Sepolia');
+
+	// Retrieve the token ID(for ERC-20)from the source chain
+	const erc20TokenAddress = 'Custom ERC-20ADDRESS'; // Custom ERC-20 Token Address
+    const erc20TokenId: TokenId = Wormhole.tokenId(origChain.chain, erc20TokenAddress);
+    console.log('ERC20 Token ID for Avalanche Sepolia (Example):', erc20TokenId);
+
+	// Retrieve the token ID(for native)from the source chain
+	// const token = await origChain.getNativeWrappedTokenId();
 
 	// Destination chain signer setup
 	const gasLimit = BigInt(2_500_000); // Optional for EVM Chains
 	const { signer: destSigner } = await getSigner(destChain, gasLimit);
 	const tbDest = await destChain.getTokenBridge();
 
+
 	// Check if the token is already wrapped on the destination chain
 	try {
-		const wrapped = await tbDest.getWrappedAsset(token);
+		const wrapped = await tbDest.getWrappedAsset(erc20TokenId);
 		console.log(`Token already wrapped on ${destChain.chain}. Skipping attestation.`);
+
 		return { chain: destChain.chain, address: wrapped };
 	} catch (e) {
 		console.log(`No wrapped token found on ${destChain.chain}. Proceeding with attestation.`);
@@ -36,7 +45,7 @@ import { getSigner } from '../helpers/helpers';
 	// Create an attestation transaction on the source chain
 	const tbOrig = await origChain.getTokenBridge();
 	const attestTxns = tbOrig.createAttestation(
-		token.address,
+		erc20TokenId.address,
 		Wormhole.parseAddress(origSigner.chain(), origSigner.address())
 	);
 
@@ -53,6 +62,7 @@ import { getSigner } from '../helpers/helpers';
 	// Fetch the signed VAA
 	const timeout = 25 * 60 * 1000;
 	const vaa = await wh.getVaa(msgs[0]!, 'TokenBridge:AttestMeta', timeout);
+
 	if (!vaa) {
 		throw new Error('VAA not found after retries exhausted. Try extending the timeout.');
 	}
@@ -75,7 +85,7 @@ import { getSigner } from '../helpers/helpers';
 	async function waitForIt() {
 		do {
 			try {
-				const wrapped = await tbDest.getWrappedAsset(token);
+				const wrapped = await tbDest.getWrappedAsset(erc20TokenId);
 				return { chain: destChain.chain, address: wrapped };
 			} catch (e) {
 				console.error('Wrapped asset not found yet. Retrying...');
